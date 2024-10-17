53e0e726cf39d6fe70970dd84176874d96986849
aljpetri <aljpetri@gmail.com>
2024-10-17 23:48:54 +0300

    Added the new plotting for the thresholds and the respective Plots

--- Results_paper_16-10-2024/Thresholds/Plot_thresholds.py
+++ Results_paper_16-10-2024/Thresholds/Plot_thresholds.py
@@ -3,6 +3,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 from datetime import timedelta
+def plot_dataset_new(dataset_name, data, outfolder):
+    print(data)
+    outfilename = dataset_name + "_T.pdf"
+    
+    # Melt the dataframe to have a separate row for each measure, making it easier to plot
+    melted_data = pd.melt(data, id_vars=['Threshold'], value_vars=['V', 'c', 'h', 'ARI'], var_name='Measure', value_name='Value')
+    
+    # Convert 'Value' column to float
+    melted_data['Value'] = melted_data['Value'].astype(float)
+    
+    # Create the plot
+    plt.figure(figsize=(15, 8))
+    sns.lineplot(x='Threshold', y='Value', hue='Measure', data=melted_data,linewidth=3)
+    
+    # Set title with larger font size
+    plt.title(dataset_name, fontsize=32)
+    
+    # Set axis labels with larger font sizes
+    plt.xlabel('Threshold', fontsize=30)
+    plt.ylabel('Cluster measures', fontsize=30)
+    
+    # Customize tick label font size
+    plt.xticks(fontsize=28, rotation=45, ha='right')
+    plt.yticks(fontsize=28)
+    
+    # Customize the legend font size and title font size
+    plt.legend(title='Measure', loc='upper left', fontsize=28, title_fontsize=30)
+    
+    # Adjust layout to prevent overlap of elements
+    plt.tight_layout()
+    
+    # Save the plot as a PDF in the specified output folder
+    plt.savefig(os.path.join(outfolder, outfilename))
+    
+    # Close the plot to free up memory
+    plt.close()
+    
 def plot_dataset(dataset_name,data,outfolder):
     print(data)
     outfilename = dataset_name + ".pdf"
@@ -30,8 +67,8 @@ def plot_dataset(dataset_name,data,outfolder):
     plt.close()
 
 # Read data from the TSV file
-file_path = "~/isONclust_analysis/Results_not_pushed/Thresholds/Thresholds.txt"
-outfolder= "/home/alexanderpetri/isONclust_analysis/Results_not_pushed/Thresholds/"
+file_path = "~/isONclust_analysis/Results_paper_16-10-2024/Thresholds/Thresholds.txt"
+outfolder= "/home/alexanderpetri/isONclust_analysis/Results_paper_16-10-2024/Thresholds/"
 df = pd.read_csv(file_path, sep='\t')
 #plot_rt(df)
 # Remove quotes and spaces from the 'rt(d-h:m:s)' column
@@ -56,11 +93,11 @@ alz_data = df[df['Dataset'] == 'ALZ']
 onth_data =df[df['Dataset'] == 'ONTh']
 dro_data = df[df['Dataset'] == 'Droso']
 #plot_dataset("SIRV", sirv_data,outfolder)
-plot_dataset("ALZ", alz_data,outfolder)
-plot_dataset("ONTh", onth_data,outfolder)
+plot_dataset_new("ALZ", alz_data,outfolder)
+plot_dataset_new("ONTh", onth_data,outfolder)
 #plot_dataset("SIM-500k", sim_data,outfolder)
 #plot_dataset("ONT",ont_data,outfolder)
-plot_dataset("Drosophila", dro_data,outfolder)
+plot_dataset_new("Drosophila", dro_data,outfolder)
 #measures = ['V', 'c', 'h', 'ARI']
 #for measure in measures:
 # Set the order of tools for plotting
