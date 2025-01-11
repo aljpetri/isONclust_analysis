dc6157faf56527d14751ca23478a430503167d60
aljpetri <aljpetri@gmail.com>
2025-01-12 00:57:49 +0200

    Added analysis for different k,w and changed ONT_human analysis to work on new cluster

---
+++ Evaluation/Different_k_w/submit_main_snakemake.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH -A naiss2024-5-55 
+#SBATCH --time=1-00:00:00
+#SBATCH -p shared
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH --job-name="isONclust_k_w"
+#SBATCH --mail-user=alexander.petri@math.su.se
+#SBATCH --mail-type=ALL
+
+#conda init bash
+set -o errexit
+
+# --forcerun minimap2_align minimap2_map bwa_mem_align strobemap_align strobemap_map accelalign_align accelalign_map bowtie2_align
+ml bioinfo-tools
+ml snakemake/5.32.2
+ml gcc/9.3.0
+ml samtools
+ml minimap2/2.28
+
+snakemake --keep-going -j 999999 --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
+
+
+
+
