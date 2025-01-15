b3faf2f03e28c40b1a5428902bdc5c5205c5e2d9
aljpetri <aljpetri@gmail.com>
2025-01-15 23:03:31 +0200

    Changed module versions in all submit_main_snakefile scripts

--- Evaluation/Original_paper_data/SIM_500/submit_main_snakemake.sh
+++ Evaluation/Original_paper_data/SIM_500/submit_main_snakemake.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #SBATCH -A naiss2024-5-55
 #SBATCH --time=5-00:00:00
-#SBATCH -p core
+#SBATCH -p shared
 #SBATCH -n 20
+#SBATCH -N 1
 # Memory per node specification is in MB. It is optional. 
 # The default limit is 3000MB per core.
-#SBATCH --job-name="IsONformDrosophila"
+#SBATCH --job-name="SIM_500"
 #SBATCH --mail-user=alexander.petri@math.su.se
 #SBATCH --mail-type=ALL
 
@@ -13,12 +14,15 @@
 set -o errexit
 
 # --forcerun minimap2_align minimap2_map bwa_mem_align strobemap_align strobemap_map accelalign_align accelalign_map bowtie2_align
-module load gcc/9.3.0
-module load bioinfo-tools
-module load minimap2/2.24-r1122
-module load samtools
+ml bioinfo-tools
+ml snakemake/5.30.1
+ml gcc/9.3.0
+ml samtools
+ml minimap2/2.28
+ml pysam/0.15.3-python3.7.2
+ml scikit-learn/0.22.1
 
-snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -C {cluster.C} -c {cluster.cpus-per-task} -N {cluster.Nodes}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail}" --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
+snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -p {cluster.p} -n {cluster.n} -N {cluster.N}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail}" --cluster-config cluster.json --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
 
 
 
--- Evaluation/Thresholds/submit_main_snakemake.sh
+++ Evaluation/Thresholds/submit_main_snakemake.sh
@@ -13,13 +13,12 @@ set -o errexit
 
 # --forcerun minimap2_align minimap2_map bwa_mem_align strobemap_align strobemap_map accelalign_align accelalign_map bowtie2_align
 ml bioinfo-tools
-ml snakemake/5.32.2
+ml snakemake/5.30.1
 ml gcc/9.3.0
 ml samtools
 ml minimap2/2.28
-ml pysam/0.16.0.1-python3.8.7
+ml pysam/0.15.3-python3.7.2
 ml scikit-learn/0.22.1
-
 snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -p {cluster.p} -n {cluster.n} -N {cluster.N}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail}" --cluster-config cluster.json --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
 
 
