6b0a32888c933f7ab5ae8ab912145e42b871f70e
aljpetri <aljpetri@gmail.com>
2025-02-09 12:52:54 +0200

    Changed partition for Rattle

--- Evaluation/PB_humanSIRV/submit_main_snakemake.sh
+++ Evaluation/PB_humanSIRV/submit_main_snakemake.sh
@@ -4,7 +4,7 @@
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH --job-name="ALZ_analysis"
+#SBATCH --job-name="PB_humanSIRV_analysis"
 #SBATCH --mail-user=alexander.petri@math.su.se
 #SBATCH --mail-type=ALL
 
@@ -20,5 +20,5 @@ ml minimap2/2.28
 ml pysam/0.15.3-python3.7.2
 ml scikit-learn/0.22.1
 
-snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -p {cluster.p} -n {cluster.n} -N {cluster.N}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail}" --cluster-config cluster.json --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
+snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -p {cluster.p} -n {cluster.n} -N {cluster.N}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail} --mem={cluster.mem}" --cluster-config cluster.json --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose 
 
